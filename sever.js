7fe9c5942eab9c237d374d5b6d0aec9851f3145c
zahidfahim9898 <zahidulislamfahim88@gmail.com>
2023-09-24 16:08:03 +0600

    introducing refresh token and type to find an user

--- sever.js
+++ sever.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -38,15 +38,15 @@ const User = mongoose.model("User", userSchema);
 //api to create user or register
 app.post("/users", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(req.body.password, salt)
-    const password = hash
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    const password = hash;
     const userObj = {
       fName: req.body.fName,
       lName: req.body.lName,
       email: req.body.email,
       password: password,
-      age: req.body.age
+      age: req.body.age,
     };
     const user = new User(userObj);
     await user.save();
@@ -57,48 +57,92 @@ app.post("/users", async (req, res) => {
   }
 });
 //api to login a user
-app.post("/users/login", async (req,res)=>{
+app.post("/users/login", async (req, res) => {
   try {
-    const {email,password}= req.body
-    const user = await User.findOne({email:email})
-    if(!user){
-      res.status(404).json({message: "user not found"})
-    } else{
-      const isValidPass= await bcrypt.compare(password, user.password)
-      if(!isValidPass){
-        res.status(401).json({message:"Wrong password"})
-      } else{
-        const token = jwt.sign({email:user.email, id:user._id},process.env.JWT_SECRET)
-        const userObj = user.toJSON()
-        userObj['accessToken']= token
+    const { email, password, type, refreshToken } = req.body;
+    if (!type) {
+      res.status(401).json({ message: "type not found" });
+    } else {
+      if (type == "email") {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          res.status(404).json({ message: "user not found" });
+        } else {
+          const isValidPass = await bcrypt.compare(password, user.password);
+          if (!isValidPass) {
+            res.status(401).json({ message: "Wrong password" });
+          } else {
+            const accessToken = jwt.sign(
+              { email: user.email, id: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "2d" }
+            );
+            const refreshToken = jwt.sign(
+              { id: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "30d" }
+            );
+            const userObj = user.toJSON();
+            userObj["accessToken"] = accessToken;
+            userObj["refreshToken"] = refreshToken;
 
-        res.status(200).json(userObj)
+            res.status(200).json(userObj);
+           }
+        }
+      } else {
+        jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+          if (err) {
+            res.status(401).json({ message: "refresh token not matched" });
+          } else {
+           const id = payload.id
+           const user = await User.findById(id)
+           if(!user){
+            res.status(401).json({ message: "unauthorized" });
+           } else{
+            const accessToken = jwt.sign(
+              { email: user.email, id: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "2d" }
+            );
+            const refreshToken = jwt.sign(
+              { id: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: "30d" }
+            );
+            const userObj = user.toJSON();
+            userObj["accessToken"] = accessToken;
+            userObj["refreshToken"] = refreshToken;
+
+            res.status(200).json(userObj);
+           }
+          }
+        });
       }
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "something went wrong" });
   }
-})
+});
 //middleware to authentication
-const authenticateToken = (req,res,next)=>{
-  const authHeader = req.headers.authorization
-  const token = authHeader && authHeader.split(' ')[1]
-  if(!token){
-    res.status(401).json({message:"unauthorized"})
-  } else{
-    jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-      if(err){
-        res.status(401).json({message:"unauthorized"})
-      } else{
-        req.user =user
-        next()
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "unauthorized" });
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        res.status(401).json({ message: "unauthorized" });
+      } else {
+        req.user = user;
+        next();
       }
-    })
+    });
   }
-}
+};
 //get an user profile
-app.get("/profile", authenticateToken,async (req,res)=>{
+app.get("/profile", authenticateToken, async (req, res) => {
   try {
     const id = req.user.id;
     const user = await User.findById(id);
@@ -111,8 +155,7 @@ app.get("/profile", authenticateToken,async (req,res)=>{
     console.error(error);
     res.status(500).json({ message: "something went wrong" });
   }
-  
-})
+});
 //api to get all the user
 app.get("/users", async (req, res) => {
   try {
@@ -156,9 +199,9 @@ app.put("/users/:id", async (req, res) => {
   }
 });
 //api to delete user
-app.delete("/users/:id", async (req, res) => {
+app.delete("/users/:id", authenticateToken, async (req, res) => {
   try {
-    const id = req.params.id;
+    const id = req.user.id;
     const user = await User.findByIdAndDelete(id);
 
     if (user) {
@@ -172,10 +215,6 @@ app.delete("/users/:id", async (req, res) => {
   }
 });
 
-// connection check
-app.get("/", (req, res) => {
-  res.json("Welcome");
-});
 // server running checker
 const port = process.env.PORT;
 app.listen(port, () => {
